9ccf86c4bd66b297780ba14fad35bd8e4d0220c4
ririson <ririson7@gmail.com>
2026-07-13 04:57:59 +0900

    [Add]pricing tab switch

--- main.js
+++ main.js
@@ -10,4 +10,35 @@ document.addEventListener("DOMContentLoaded", function () {
         hamburgerBtn.setAttribute('aria-expanded', String(!isExpanded));
     });
 
+    const tabButtons = document.querySelectorAll('[role="tab"]');
+    const tabPanels = document.querySelectorAll('[role="tabpanel"]');
+
+    tabButtons.forEach((tabBtn) => {
+        tabBtn.addEventListener('click', () => {
+
+            console.log("あああ");
+            const { target } = tabBtn.dataset;
+
+            for (const btn of tabButtons) {
+                btn.classList.remove('is-active');
+                btn.setAttribute('aria-selected', 'false');
+            }
+
+            for (const panel of tabPanels) {
+                console.log(panel);
+                panel.classList.add('is-hidden');
+                panel.setAttribute('aria-hidden', 'true');
+            }
+
+            tabBtn.classList.add('is-active');
+            tabBtn.setAttribute('aria-selected', 'true');
+
+            const targetPanel = document.querySelector(`#plan-${target}`);
+
+            targetPanel?.classList.remove('is-hidden');
+            targetPanel?.setAttribute('aria-hidden', 'false');
+        });
+    });
+
+
 });
